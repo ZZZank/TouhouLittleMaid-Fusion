ccc0b08c23899476dea6f7ba7b47492aa2bfd348
ZZZank <47418975+ZZZank@users.noreply.github.com>
2026-09-17 10:01:38 +0800

    init

---
+++ src/main/java/zank/mods/touhou_little_maid_fusion/TouhouLittleMaidFusionClient.java
@@ -0,0 +1,31 @@
+package zank.mods.touhou_little_maid_fusion;
+
+import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+// This class will not load on dedicated servers. Accessing client side code from here is safe.
+@Mod(value = TouhouLittleMaidFusion.MODID, dist = Dist.CLIENT)
+// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+@EventBusSubscriber(modid = TouhouLittleMaidFusion.MODID, value = Dist.CLIENT)
+public class TouhouLittleMaidFusionClient {
+    public TouhouLittleMaidFusionClient(ModContainer container) {
+        // Allows NeoForge to create a config screen for this mod's configs.
+        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
+        // Do not forget to add translations for your config options to the en_us.json file.
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event) {
+        // Some client setup code
+        TouhouLittleMaidFusion.LOGGER.info("HELLO FROM CLIENT SETUP");
+        TouhouLittleMaidFusion.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+    }
+}
